aca5608d434465c29b55fc06da69da83b14b16ab
Edufcc <edubcc15@gmail.com>
2023-08-19 11:18:19 -0300

    styles box

--- src/pages/portfolio/portfolio.tsx
+++ src/pages/portfolio/portfolio.tsx
@@ -6,19 +6,12 @@ import Title from "../../components/title/title"
 export function Portfolio() {
     return (
         <>
-            <section className={styles.container}>
+            <section id="projects" className={styles.container}>
 
                 <div className={styles.portfolio}>
                     <Title>Portfólio</Title>
 
                     <div className={styles.content}>
-                        <div className={styles.infoProject} >
-                            <img className={styles.picDeveloper} src={picDeveloper} alt="" />
-                            <div className={styles.text}>
-                                <p className={styles.subtitle}>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos</p>
-                            </div>
-                            
-                        </div>
                         <div className={styles.infoProject} >
                             <img className={styles.picDeveloper} src={picDeveloper} alt="" />
                             <div className={styles.text}>
